63ab501045d02ec07be05bfc8fef828cd17c1bf9
Uğurcan Yılmaz <101149351+yilmazzugurcan@users.noreply.github.com>
2022-04-11 23:04:33 +0300

    Add files via upload

---
+++ KucuktenBuyugeSiralama.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+
+public class KucuktenBuyugeSiralama {
+
+    public static void main(String[] args) {
+
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("First number: ");
+        a = input.nextInt();
+
+        System.out.print("Second number: ");
+        b = input.nextInt();
+
+        System.out.print("Third number: ");
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c)
+            {
+                System.out.println("a > b > c");
+            } else
+            {
+                System.out.println("a > c > b");
+            }
+        }
+        else if ((b > a) && (b > c))
+        {
+            if (a > c)
+            {
+                System.out.println("b > a > c");
+            } else
+            {
+                System.out.println("b > c > a");
+            }
+        }
+        else
+        {
+            if (b > a)
+            {
+                System.out.println("c > b > a");
+            } else
+            {
+                System.out.println("c > a > b");
+            }
+        }
+    }
+}
