e3a1edc07246167a63c6baa1dd7a8fd9b3c13134
Uğurcan Yılmaz <101149351+yilmazzugurcan@users.noreply.github.com>
2022-04-19 21:08:05 +0300

    Add files via upload

---
+++ RakamToplami.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class RakamToplami {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+
+        try {
+            int n = input.nextInt();
+            System.out.println("Girdiğiniz sayının rakamları toplamı: " + sumDigits(n));
+        }catch (Exception e) {
+            System.err.println("Hatalı değer girdiniz!");
+        }finally {
+            input.close();
+        }
+    }
+
+    public static int sumDigits(int n) {
+        int total = 0;
+
+        while(n != 0) {
+            total += n % 10;
+            n /= 10;
+        }
+
+        return total;
+    }
+
+}
