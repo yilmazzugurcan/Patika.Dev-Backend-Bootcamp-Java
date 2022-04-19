a8c5b7090fe015c8df8d660f42278d8e37683ae0
Uğurcan Yılmaz <101149351+yilmazzugurcan@users.noreply.github.com>
2022-04-19 21:43:23 +0300

    Add files via upload

---
+++ AsalSayiBulma.java
@@ -0,0 +1,20 @@
+public class AsalSayiBulma {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++) {
+            if(isPrime(i))
+                System.out.print(i + " ");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if(n<2)
+            return false;
+
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if(n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
